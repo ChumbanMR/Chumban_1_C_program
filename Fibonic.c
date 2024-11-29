3e4d85e1dc927427c8c0f5f57d76b7af46c7e36e
ChumbanMR <chumbichumbichumban@gmail.com>
2024-11-29 15:00:22 +0530

    Fibonic.c

---
+++ Fibonic.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int n, t1=0, t2=1, nextterm;
+printf("Enter the number of term:");
+scanf("%d",&n);
+printf("Fibonacci series:%d,%d",t1,t2);
+for(int i=3; i<=n; i++){
+nextterm=t1+t2;
+printf("%d",nextterm);
+t1=t2;
+t2=nextterm;
+}
+printf("chumabn.MR");
+return 0;
+}
